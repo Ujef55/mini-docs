f555517436d7a3fcd52024702330fb74e62fc7fb
Ujef55 <ujef.kelawala123@gmail.com>
2024-02-20 18:06:40 +0530

    working on card component

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { FaFileLines } from "react-icons/fa6";
 import { MdOutlineFileDownload } from "react-icons/md";
+import { IoClose } from "react-icons/io5";
 
 
-function Cards() {
+
+function Cards({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/60 px-8 py-10 overflow-hidden text-white'>
+    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/80 px-8 py-10 overflow-hidden text-white'>
         <FaFileLines />
         <p className='text-sm mt-5 font-semibold leading-tight'>
-            Lorem ipsum dolor sit amet consectetur.
+            {data.desc}
         </p>
-        <div className='card-footer absolute bottom-0 h-12 py-3 px-8 w-full left-0'>
-            <div className='flex items-center justify-between mb-5'>
-                <h5>.6mb</h5>
-                <span className='w-5 h-5 bg-sky-200 rounded-full flex items-center justify-center'>
-                    <MdOutlineFileDownload className='cursor-pointer' />
+        <div className='card-footer absolute bottom-0 w-full left-0'>
+            <div className='flex items-center justify-between px-8 py-3 mb-3'>
+                <h5>{data.filesize}</h5>
+                <span className='w-5 h-5 bg-zinc-700 rounded-full flex items-center justify-center'>
+                    {data.closeIcon ? <IoClose className='cursor-pointer' size='.7em' color='#fff'  /> : <MdOutlineFileDownload className='cursor-pointer' size='.7em' color='#fff' />}  
                 </span>
             </div>
+            {data.tag.isOpen && (
+                    <div className={`tag w-full py-4 bg-${data.tag.tagColor}-600 px-8 flex items-center justify-center`}>
+                        <h3 className='cursor-pointer'>{data.tag.tagTitle}</h3>
+                    </div>
+            )}
         </div>
     </div>
   )
--- src/Components/Foreground.jsx
+++ src/Components/Foreground.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import Cards from './Cards'
 
 function Foreground() {
+  const data = [{
+    desc: ' Lorem ipsum dolor sit amet consectetur.',
+    filesize: '.7mb',
+    closeIcon: true,
+    tag: {isOpen: true, tagTitle: 'Download now', tagColor: 'green'}
+  },
+  {
+    desc: ' Lorem ipsum dolor sit amet consectetur.',
+    filesize: '.7mb',
+    closeIcon: true,
+    tag: {isOpen: true, tagTitle: 'Download now', tagColor: 'green'}
+  },
+  {
+    desc: ' Lorem ipsum dolor sit amet consectetur.',
+    filesize: '.7mb',
+    closeIcon: true,
+    tag: {isOpen: true, tagTitle: 'Download now', tagColor: 'green'}
+  },
+];
   return (
-    <div className='fixed top-0 left-0 w-full z-[3] h-full bg-sky-600/20'>
-        <Cards />
+    <div className='fixed top-0 left-0 w-full z-[3] h-full p-5 flex gap-5 flex-wrap'>
+        {data.map((item, index) => (
+          <Cards data={item} />
+        ))}
     </div>
   )
 }
